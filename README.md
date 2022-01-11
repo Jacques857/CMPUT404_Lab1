0256ecef58086207815aefa44869d2ba0cc16bea
Jacques857 <jleongsi@ualberta.ca>
2022-01-10 20:53:40 -0500

    updated references and added source code download/print to the script

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 ## References:
 * https://note.nkmk.me/en/python-package-version/
 * https://docs.python-requests.org/en/latest/
+* Martijn Pieters, https://stackoverflow.com/questions/14120502/how-to-download-and-write-a-file-from-github-using-requests
+* https://www.tutorialspoint.com/downloading-files-from-web-using-python
--- script.py
+++ script.py
@@ -1,4 +1,11 @@
 import requests
-print(requests.__version__)
+print(requests.__version__, "\n\n")
+
 r = requests.get('http://www.google.com/')
-print("Status: ", r.status_code, "\n", "Page contents:\n", r.text)
+print("Status: ", r.status_code, "\n", "Page contents:\n", r.text, "\n\n")
+
+r = requests.get('https://raw.githubusercontent.com/Jacques857/CMPUT404_Lab1/main/script.py')
+
+open('downloadedScript.py', 'wb').write(r.content)
+
+print("Source code:\n" + r.text)
